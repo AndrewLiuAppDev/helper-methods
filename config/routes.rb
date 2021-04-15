fc0c39c070f12b1695adda97f15ca662ae1bdb71
yanyuliu92 <yliu33@chicagobooth.edu>
2021-04-15 07:42:57 +0000

    Refactoring 8

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   root "movies#index"
 
   # Routes for the Movie resource:
-
-  # CREATE
-  post "/movies" => "movies#create", as: :movies
-  get "/movies/new" =>  "movies#new", as: :new_movies
+  resources :movies
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies
+  # get "/movies/new" =>  "movies#new", as: :new_movies
           
-  # READ
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show", as: :movie
+  # # READ
+  # get "/movies" => "movies#index"
+  # get "/movies/:id" => "movies#show", as: :movie
   
-  # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movies
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movies
   
-  # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy"
 
-  #------------------------------
+  # #------------------------------
 end
